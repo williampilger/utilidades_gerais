e33793a3eaefd40d9a93607df83b59a8bd196e0a
William Pilger <40075505+williampilger@users.noreply.github.com>
2021-07-27 23:03:52 -0300

    Update ContadorDeAtividades.py

--- ContadorDeAtividades/ContadorDeAtividades.py
+++ ContadorDeAtividades/ContadorDeAtividades.py
@@ -1,15 +1,35 @@
 import os
-def restart_program():
-    os.system(f"python \"{__file__}\"")
-    quit()
-from datetime import datetime
+import platform
 import time
-try:
-    from win32gui import GetWindowText, GetForegroundWindow #para instalar use >>> pip install pywin32
-except:
-    print("\nINSTALANDO BIBLIOTECA NECESSÁRIA, AGUARDE!\n(CONEXÃO COM INTERNET NECESSÁRIA)")
-    os.system("pip install pywin32")
-    restart_program()
+from datetime import datetime
+
+eh_windows = platform.system() == "Windows"
+eh_linux = platform.system() == "Linux"
+
+def restart_program():
+    if(eh_windows):
+        os.system(f"python \"{__file__}\"")
+    elif(eh_linux):
+        os.system(f"python3 \"{__file__}\"")
+    quit()
+
+if(eh_windows):#includes especiais do Windows
+    try:
+        from win32gui import GetWindowText, GetForegroundWindow #para instalar use >>> pip install pywin32
+    except:
+        print("\nINSTALANDO BIBLIOTECA NECESSÁRIA, AGUARDE!\n(CONEXÃO COM INTERNET NECESSÁRIA)")
+        os.system("pip install pywin32")
+        restart_program()
+
+def obtem_nome_janela():
+    if(eh_windows):
+        return GetWindowText(GetForegroundWindow())
+
+def limpar_terminal():
+    if(eh_windows):
+        os.system("cls")
+    elif(eh_linux):
+        os.system("clear")
 
 def registra_log_geral(texto):
     with open("log_geral.txt", "a") as arquivo:
@@ -17,17 +37,42 @@ def registra_log_geral(texto):
         arquivo.writelines(f"\n{instante}\t{texto}")
     return
 
-def inicio():
+def monitoracao():
     while True:
-        nome_janela = GetWindowText(GetForegroundWindow())
-        registra_log_geral(nome_janela)
+        registra_log_geral(obtem_nome_janela())
         time.sleep(1)
+
+def menu_principal():
+    limpar_terminal()
+    print("\n\n")
+    while True:
+        print("Iniciando aplicação!\n\n     By: William Pilger\n\n\n Contador de horas!")
+        print("\n\n    (1) - Cadastrar projetos\n    (2) - Iniciar monitoração\n    (3) - Ver minha atividade")
+        opcao = int(input("Para começarmos, digite sua opção:"))
+        if (opcao == 1):
+            return #sair da aplicação
+        elif (opcao == 1):
+            monitoracao()
+            return #sair da aplicação
+        elif (opcao == 2):
+            return #sair da aplicação
+        limpar_terminal()
+        print("\nOPÇÃO INVÁLIDA\n")
+
+
+def inicio():
+    if(eh_windows):
+        menu_principal()
+    else:
+        print("\n\nLamentamos, mas esta aplicação ainda não é compatível com o seu sistema operacional!\nA aplicação será finalizada!")
+        time.sleep(5)
+        quit()
 
 if (__name__ == "__main__"):
     inicio()
-    print("FIM DO PROGRAMA!")
-    #input()
-    
+    print("FIM DA EXECUÇÃO!")
+
+
 '''def ler_projetos():
     with open("projetos.txt", "r") as arquivo:
         projetos = []
