eb87853905c1d6b2580ef6d93d9741b3093e19e9
William Pilger <40075505+williampilger@users.noreply.github.com>
2026-04-24 13:37:42 -0300

    Update create-envs-backup.sh

--- LinuxScripts/create-envs-backup.sh
+++ LinuxScripts/create-envs-backup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Script para criar uma cópia de todos os `.env*` no diretório escolhido para outro.
+#  - Toda a árvore de pastas é criada
+#  - Todos arquivos .env são copiados (.env.prod, .env.sample, etc...)
+
 # --- Configurações ---
-ORIGEM="/media/wilger/OS/github"
+ORIGEM="/media/caminho/do/seu/github" #caminho que deve ser varrido
 DESTINO="." # Diretório atual
 
 echo "Iniciando backup dos arquivos .env..."
 
 rsync -amv --include='*/' --include='.env*' --exclude='*' "$ORIGEM" "$DESTINO"
 
-echo "✅ Backup concluído de '$ORIGEM' para '$DESTINO'"
+echo "Backup dos ENV files concluído de '$ORIGEM' para '$DESTINO'"
